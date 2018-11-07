098700b475cdc8dbaa64b96c39d3eb25a3cb3e27
lzz <liuzizhang1994@gmail.com>
2018-11-07 21:20:31 +0800

    fix toaster init bug

--- gymclub/app/src/main/java/top/koguma/gymclub/GymClubApp.java
+++ gymclub/app/src/main/java/top/koguma/gymclub/GymClubApp.java
@@ -2,6 +2,12 @@ package top.koguma.gymclub;
 
 import android.app.Application;
 import com.laputapp.Laputapp;
+import top.koguma.gymclub.utils.Toaster;
 
 public class GymClubApp extends Laputapp {
+
+    @Override public void onCreate() {
+        super.onCreate();
+        Toaster.init(this);
+    }
 }
