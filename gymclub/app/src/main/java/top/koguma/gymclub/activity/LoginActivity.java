2906c842cb201405a94b21b5626f5533a7b4b6dd
lzz <liuzizhang1994@gmail.com>
2018-11-07 21:17:36 +0800

    finish assginment1

---
+++ gymclub/app/src/main/java/top/koguma/gymclub/activity/LoginActivity.java
@@ -0,0 +1,28 @@
+package top.koguma.gymclub.activity;
+
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import top.koguma.gymclub.R;
+
+public class LoginActivity extends GymClubBaseActivity {
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    @Override protected void onSetupToolbar(Toolbar toolbar) {
+        super.onSetupToolbar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- gymclub/app/src/main/java/top/koguma/gymclub/fragment/ProfileFragment.java
+++ gymclub/app/src/main/java/top/koguma/gymclub/fragment/ProfileFragment.java
@@ -1,6 +1,48 @@
 package top.koguma.gymclub.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import com.laputapp.ui.BaseFragment;
+import top.koguma.gymclub.Navigator;
+import top.koguma.gymclub.R;
+import top.koguma.gymclub.utils.Toaster;
 
 public class ProfileFragment extends GymClubBaseFragment {
+
+    @Nullable @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile, null);
+    }
+
+    @Override protected void onSetupToolbar(Toolbar toolbar) {
+        super.onSetupToolbar(toolbar);
+        setHasOptionsMenu(true);
+    }
+
+    @Override public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.profile, menu);
+    }
+
+    @Override public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.profile_register:
+                Navigator.startRegisterActivity(getContext());
+                break;
+            case R.id.profile_Login:
+                Navigator.startLoginActivity(getContext());
+                break;
+            case R.id.profile_logout:
+                Toaster.showToast("Logout!");
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
