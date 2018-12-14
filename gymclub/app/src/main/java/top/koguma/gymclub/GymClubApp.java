089080307926bf153950659a25c8b34bf94d5441
lzz <liuzizhang1994@gmail.com>
2018-12-14 19:44:29 +0800

    add bmob sdk

--- gymclub/app/src/main/java/top/koguma/gymclub/GymClubApp.java
+++ gymclub/app/src/main/java/top/koguma/gymclub/GymClubApp.java
@@ -1,6 +1,7 @@
 package top.koguma.gymclub;
 
 import android.app.Application;
+import cn.bmob.v3.Bmob;
 import com.laputapp.Laputapp;
 import top.koguma.gymclub.utils.Toaster;
 
@@ -9,5 +10,6 @@ public class GymClubApp extends Laputapp {
     @Override public void onCreate() {
         super.onCreate();
         Toaster.init(this);
+        Bmob.initialize(this, BuildConfig.BMOB_APP_ID);
     }
 }
